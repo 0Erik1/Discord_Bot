2b60aca19c058b653439a8c08a4b3770f12814f2
0Erik1 <erikpinheiroalves@gmail.com>
2025-10-11 20:29:07 -0300

    queue for the songs

--- bot/cogs/music.py
+++ bot/cogs/music.py
@@ -9,6 +9,7 @@ ytdl_opts = {
     'quiet': True,
     'extract_flat': False,
     'ignoreerrors': True,
+    'outtmpl': 'bot/data/songs/%(title)s.%(ext)s'
 }
 
 ytdl = youtube_dl.YoutubeDL(ytdl_opts)
@@ -16,9 +17,33 @@ ytdl = youtube_dl.YoutubeDL(ytdl_opts)
 class Music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.queue = []
+        self.index = 0
 
     @commands.command()
     async def play(self, ctx, url):
+
+        # Função para apagar o arquivo depois que terminar
+        def after_playing(error):
+            if error:
+                print("erro ao tocar a musica")
+
+            self.index +=1
+            if self.index < len(self.queue):
+                info = ytdl.extract_info(self.queue[self.index], download=True) #sempre baixa o atual
+                filename = ytdl.prepare_filename(info)  # caminho do arquivo baixado
+            
+                # toca a música
+                source = discord.FFmpegPCMAudio(filename)
+                voice_client.play(source, after=after_playing)
+            else:
+                folder = "bot/data/songs"
+
+                for filename in os.listdir(folder):
+                    file_path = os.path.join(folder, filename)
+                    if os.path.isfile(file_path):
+                        os.remove(file_path)
+        #verifica se está em um canal para conectar
         if not ctx.voice_client:
             if ctx.author.voice:
                 await ctx.author.voice.channel.connect()
@@ -27,23 +52,18 @@ class Music(commands.Cog):
                 return
 
         voice_client = ctx.voice_client
+        self.queue.append(url) #adiciona url a lista
 
-        # Baixa o áudio
-        info = ytdl.extract_info(url, download=True)
-        filename = ytdl.prepare_filename(info)  # caminho do arquivo baixado
+        if not voice_client.is_playing():
 
-        # Função para apagar o arquivo depois que terminar
-        def after_playing(error):
-            try:
-                os.remove(filename)
-            except Exception as e:
-                print(f"Erro ao remover arquivo: {e}")
+            # Baixa o áudio
+            info = ytdl.extract_info(self.queue[self.index], download=True) #sempre baixa o atual
+            filename = ytdl.prepare_filename(info)  # caminho do arquivo baixado
 
-        # Toca o arquivo
-        source = discord.FFmpegPCMAudio(filename)
-        voice_client.play(source, after=after_playing)
-
-        await ctx.send(f"Tocando agora: {info['title']}")
+            # toca a música
+            source = discord.FFmpegPCMAudio(filename)
+            voice_client.play(source, after=after_playing)
+            await ctx.send(f"Tocando agora: {info['title']}")
 
     @commands.command()
     async def stop(self, ctx):
